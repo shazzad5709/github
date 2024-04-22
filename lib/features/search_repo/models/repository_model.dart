7b3b23924bc0689e40a521081505ee2baddec988
shazzad5709 <shazzad1111@gmail.com>
2024-04-22 18:25:50 +0600

    Add Folder Structure, Repository Model

---
+++ lib/features/search_repo/models/repository_model.dart
@@ -0,0 +1,465 @@
+import 'dart:convert';
+
+RepositoryModel repositoryModelFromJson(String str) => RepositoryModel.fromJson(json.decode(str));
+
+String repositoryModelToJson(RepositoryModel data) => json.encode(data.toJson());
+
+class RepositoryModel {
+    int totalCount;
+    bool incompleteResults;
+    List<Item> items;
+
+    RepositoryModel({
+        required this.totalCount,
+        required this.incompleteResults,
+        required this.items,
+    });
+
+    factory RepositoryModel.fromJson(Map<String, dynamic> json) => RepositoryModel(
+        totalCount: json["total_count"],
+        incompleteResults: json["incomplete_results"],
+        items: List<Item>.from(json["items"].map((x) => Item.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "total_count": totalCount,
+        "incomplete_results": incompleteResults,
+        "items": List<dynamic>.from(items.map((x) => x.toJson())),
+    };
+}
+
+class Item {
+    int id;
+    String nodeId;
+    String name;
+    String fullName;
+    Owner owner;
+    bool private;
+    String htmlUrl;
+    String description;
+    bool fork;
+    String url;
+    DateTime createdAt;
+    DateTime updatedAt;
+    DateTime pushedAt;
+    String homepage;
+    int size;
+    int stargazersCount;
+    int watchersCount;
+    String language;
+    int forksCount;
+    int openIssuesCount;
+    String masterBranch;
+    String defaultBranch;
+    int score;
+    String archiveUrl;
+    String assigneesUrl;
+    String blobsUrl;
+    String branchesUrl;
+    String collaboratorsUrl;
+    String commentsUrl;
+    String commitsUrl;
+    String compareUrl;
+    String contentsUrl;
+    String contributorsUrl;
+    String deploymentsUrl;
+    String downloadsUrl;
+    String eventsUrl;
+    String forksUrl;
+    String gitCommitsUrl;
+    String gitRefsUrl;
+    String gitTagsUrl;
+    String gitUrl;
+    String issueCommentUrl;
+    String issueEventsUrl;
+    String issuesUrl;
+    String keysUrl;
+    String labelsUrl;
+    String languagesUrl;
+    String mergesUrl;
+    String milestonesUrl;
+    String notificationsUrl;
+    String pullsUrl;
+    String releasesUrl;
+    String sshUrl;
+    String stargazersUrl;
+    String statusesUrl;
+    String subscribersUrl;
+    String subscriptionUrl;
+    String tagsUrl;
+    String teamsUrl;
+    String treesUrl;
+    String cloneUrl;
+    String mirrorUrl;
+    String hooksUrl;
+    String svnUrl;
+    int forks;
+    int openIssues;
+    int watchers;
+    bool hasIssues;
+    bool hasProjects;
+    bool hasPages;
+    bool hasWiki;
+    bool hasDownloads;
+    bool archived;
+    bool disabled;
+    String visibility;
+    License license;
+
+    Item({
+        required this.id,
+        required this.nodeId,
+        required this.name,
+        required this.fullName,
+        required this.owner,
+        required this.private,
+        required this.htmlUrl,
+        required this.description,
+        required this.fork,
+        required this.url,
+        required this.createdAt,
+        required this.updatedAt,
+        required this.pushedAt,
+        required this.homepage,
+        required this.size,
+        required this.stargazersCount,
+        required this.watchersCount,
+        required this.language,
+        required this.forksCount,
+        required this.openIssuesCount,
+        required this.masterBranch,
+        required this.defaultBranch,
+        required this.score,
+        required this.archiveUrl,
+        required this.assigneesUrl,
+        required this.blobsUrl,
+        required this.branchesUrl,
+        required this.collaboratorsUrl,
+        required this.commentsUrl,
+        required this.commitsUrl,
+        required this.compareUrl,
+        required this.contentsUrl,
+        required this.contributorsUrl,
+        required this.deploymentsUrl,
+        required this.downloadsUrl,
+        required this.eventsUrl,
+        required this.forksUrl,
+        required this.gitCommitsUrl,
+        required this.gitRefsUrl,
+        required this.gitTagsUrl,
+        required this.gitUrl,
+        required this.issueCommentUrl,
+        required this.issueEventsUrl,
+        required this.issuesUrl,
+        required this.keysUrl,
+        required this.labelsUrl,
+        required this.languagesUrl,
+        required this.mergesUrl,
+        required this.milestonesUrl,
+        required this.notificationsUrl,
+        required this.pullsUrl,
+        required this.releasesUrl,
+        required this.sshUrl,
+        required this.stargazersUrl,
+        required this.statusesUrl,
+        required this.subscribersUrl,
+        required this.subscriptionUrl,
+        required this.tagsUrl,
+        required this.teamsUrl,
+        required this.treesUrl,
+        required this.cloneUrl,
+        required this.mirrorUrl,
+        required this.hooksUrl,
+        required this.svnUrl,
+        required this.forks,
+        required this.openIssues,
+        required this.watchers,
+        required this.hasIssues,
+        required this.hasProjects,
+        required this.hasPages,
+        required this.hasWiki,
+        required this.hasDownloads,
+        required this.archived,
+        required this.disabled,
+        required this.visibility,
+        required this.license,
+    });
+
+    factory Item.fromJson(Map<String, dynamic> json) => Item(
+        id: json["id"],
+        nodeId: json["node_id"],
+        name: json["name"],
+        fullName: json["full_name"],
+        owner: Owner.fromJson(json["owner"]),
+        private: json["private"],
+        htmlUrl: json["html_url"],
+        description: json["description"],
+        fork: json["fork"],
+        url: json["url"],
+        createdAt: DateTime.parse(json["created_at"]),
+        updatedAt: DateTime.parse(json["updated_at"]),
+        pushedAt: DateTime.parse(json["pushed_at"]),
+        homepage: json["homepage"],
+        size: json["size"],
+        stargazersCount: json["stargazers_count"],
+        watchersCount: json["watchers_count"],
+        language: json["language"],
+        forksCount: json["forks_count"],
+        openIssuesCount: json["open_issues_count"],
+        masterBranch: json["master_branch"],
+        defaultBranch: json["default_branch"],
+        score: json["score"],
+        archiveUrl: json["archive_url"],
+        assigneesUrl: json["assignees_url"],
+        blobsUrl: json["blobs_url"],
+        branchesUrl: json["branches_url"],
+        collaboratorsUrl: json["collaborators_url"],
+        commentsUrl: json["comments_url"],
+        commitsUrl: json["commits_url"],
+        compareUrl: json["compare_url"],
+        contentsUrl: json["contents_url"],
+        contributorsUrl: json["contributors_url"],
+        deploymentsUrl: json["deployments_url"],
+        downloadsUrl: json["downloads_url"],
+        eventsUrl: json["events_url"],
+        forksUrl: json["forks_url"],
+        gitCommitsUrl: json["git_commits_url"],
+        gitRefsUrl: json["git_refs_url"],
+        gitTagsUrl: json["git_tags_url"],
+        gitUrl: json["git_url"],
+        issueCommentUrl: json["issue_comment_url"],
+        issueEventsUrl: json["issue_events_url"],
+        issuesUrl: json["issues_url"],
+        keysUrl: json["keys_url"],
+        labelsUrl: json["labels_url"],
+        languagesUrl: json["languages_url"],
+        mergesUrl: json["merges_url"],
+        milestonesUrl: json["milestones_url"],
+        notificationsUrl: json["notifications_url"],
+        pullsUrl: json["pulls_url"],
+        releasesUrl: json["releases_url"],
+        sshUrl: json["ssh_url"],
+        stargazersUrl: json["stargazers_url"],
+        statusesUrl: json["statuses_url"],
+        subscribersUrl: json["subscribers_url"],
+        subscriptionUrl: json["subscription_url"],
+        tagsUrl: json["tags_url"],
+        teamsUrl: json["teams_url"],
+        treesUrl: json["trees_url"],
+        cloneUrl: json["clone_url"],
+        mirrorUrl: json["mirror_url"],
+        hooksUrl: json["hooks_url"],
+        svnUrl: json["svn_url"],
+        forks: json["forks"],
+        openIssues: json["open_issues"],
+        watchers: json["watchers"],
+        hasIssues: json["has_issues"],
+        hasProjects: json["has_projects"],
+        hasPages: json["has_pages"],
+        hasWiki: json["has_wiki"],
+        hasDownloads: json["has_downloads"],
+        archived: json["archived"],
+        disabled: json["disabled"],
+        visibility: json["visibility"],
+        license: License.fromJson(json["license"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "node_id": nodeId,
+        "name": name,
+        "full_name": fullName,
+        "owner": owner.toJson(),
+        "private": private,
+        "html_url": htmlUrl,
+        "description": description,
+        "fork": fork,
+        "url": url,
+        "created_at": createdAt.toIso8601String(),
+        "updated_at": updatedAt.toIso8601String(),
+        "pushed_at": pushedAt.toIso8601String(),
+        "homepage": homepage,
+        "size": size,
+        "stargazers_count": stargazersCount,
+        "watchers_count": watchersCount,
+        "language": language,
+        "forks_count": forksCount,
+        "open_issues_count": openIssuesCount,
+        "master_branch": masterBranch,
+        "default_branch": defaultBranch,
+        "score": score,
+        "archive_url": archiveUrl,
+        "assignees_url": assigneesUrl,
+        "blobs_url": blobsUrl,
+        "branches_url": branchesUrl,
+        "collaborators_url": collaboratorsUrl,
+        "comments_url": commentsUrl,
+        "commits_url": commitsUrl,
+        "compare_url": compareUrl,
+        "contents_url": contentsUrl,
+        "contributors_url": contributorsUrl,
+        "deployments_url": deploymentsUrl,
+        "downloads_url": downloadsUrl,
+        "events_url": eventsUrl,
+        "forks_url": forksUrl,
+        "git_commits_url": gitCommitsUrl,
+        "git_refs_url": gitRefsUrl,
+        "git_tags_url": gitTagsUrl,
+        "git_url": gitUrl,
+        "issue_comment_url": issueCommentUrl,
+        "issue_events_url": issueEventsUrl,
+        "issues_url": issuesUrl,
+        "keys_url": keysUrl,
+        "labels_url": labelsUrl,
+        "languages_url": languagesUrl,
+        "merges_url": mergesUrl,
+        "milestones_url": milestonesUrl,
+        "notifications_url": notificationsUrl,
+        "pulls_url": pullsUrl,
+        "releases_url": releasesUrl,
+        "ssh_url": sshUrl,
+        "stargazers_url": stargazersUrl,
+        "statuses_url": statusesUrl,
+        "subscribers_url": subscribersUrl,
+        "subscription_url": subscriptionUrl,
+        "tags_url": tagsUrl,
+        "teams_url": teamsUrl,
+        "trees_url": treesUrl,
+        "clone_url": cloneUrl,
+        "mirror_url": mirrorUrl,
+        "hooks_url": hooksUrl,
+        "svn_url": svnUrl,
+        "forks": forks,
+        "open_issues": openIssues,
+        "watchers": watchers,
+        "has_issues": hasIssues,
+        "has_projects": hasProjects,
+        "has_pages": hasPages,
+        "has_wiki": hasWiki,
+        "has_downloads": hasDownloads,
+        "archived": archived,
+        "disabled": disabled,
+        "visibility": visibility,
+        "license": license.toJson(),
+    };
+}
+
+class License {
+    String key;
+    String name;
+    String url;
+    String spdxId;
+    String nodeId;
+    String htmlUrl;
+
+    License({
+        required this.key,
+        required this.name,
+        required this.url,
+        required this.spdxId,
+        required this.nodeId,
+        required this.htmlUrl,
+    });
+
+    factory License.fromJson(Map<String, dynamic> json) => License(
+        key: json["key"],
+        name: json["name"],
+        url: json["url"],
+        spdxId: json["spdx_id"],
+        nodeId: json["node_id"],
+        htmlUrl: json["html_url"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "key": key,
+        "name": name,
+        "url": url,
+        "spdx_id": spdxId,
+        "node_id": nodeId,
+        "html_url": htmlUrl,
+    };
+}
+
+class Owner {
+    String login;
+    int id;
+    String nodeId;
+    String avatarUrl;
+    String gravatarId;
+    String url;
+    String receivedEventsUrl;
+    String type;
+    String htmlUrl;
+    String followersUrl;
+    String followingUrl;
+    String gistsUrl;
+    String starredUrl;
+    String subscriptionsUrl;
+    String organizationsUrl;
+    String reposUrl;
+    String eventsUrl;
+    bool siteAdmin;
+
+    Owner({
+        required this.login,
+        required this.id,
+        required this.nodeId,
+        required this.avatarUrl,
+        required this.gravatarId,
+        required this.url,
+        required this.receivedEventsUrl,
+        required this.type,
+        required this.htmlUrl,
+        required this.followersUrl,
+        required this.followingUrl,
+        required this.gistsUrl,
+        required this.starredUrl,
+        required this.subscriptionsUrl,
+        required this.organizationsUrl,
+        required this.reposUrl,
+        required this.eventsUrl,
+        required this.siteAdmin,
+    });
+
+    factory Owner.fromJson(Map<String, dynamic> json) => Owner(
+        login: json["login"],
+        id: json["id"],
+        nodeId: json["node_id"],
+        avatarUrl: json["avatar_url"],
+        gravatarId: json["gravatar_id"],
+        url: json["url"],
+        receivedEventsUrl: json["received_events_url"],
+        type: json["type"],
+        htmlUrl: json["html_url"],
+        followersUrl: json["followers_url"],
+        followingUrl: json["following_url"],
+        gistsUrl: json["gists_url"],
+        starredUrl: json["starred_url"],
+        subscriptionsUrl: json["subscriptions_url"],
+        organizationsUrl: json["organizations_url"],
+        reposUrl: json["repos_url"],
+        eventsUrl: json["events_url"],
+        siteAdmin: json["site_admin"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "login": login,
+        "id": id,
+        "node_id": nodeId,
+        "avatar_url": avatarUrl,
+        "gravatar_id": gravatarId,
+        "url": url,
+        "received_events_url": receivedEventsUrl,
+        "type": type,
+        "html_url": htmlUrl,
+        "followers_url": followersUrl,
+        "following_url": followingUrl,
+        "gists_url": gistsUrl,
+        "starred_url": starredUrl,
+        "subscriptions_url": subscriptionsUrl,
+        "organizations_url": organizationsUrl,
+        "repos_url": reposUrl,
+        "events_url": eventsUrl,
+        "site_admin": siteAdmin,
+    };
+}
